af69c7803848e2f932562cbcf00cc9e8f9f788a1
radecoder <mohdsahildw@gmail.com>
2025-04-19 02:19:46 +0530

    project 1 complete

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,14 @@
+import Navigation from './components/navigation';
+import HeroSection from './components/hero';
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+      <HeroSection/>
+      
+    </div>
+  );
+};
+
+export default App;
